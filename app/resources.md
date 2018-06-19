e7e8281c7743789ec405010da69d8cf54683678a
SerggioC <sergiocrz@gmail.com>
2018-06-19 17:45:49 +0100

    Bottom Navigation

---
+++ app/resources.md
@@ -0,0 +1,15 @@
+
+https://github.com/twitter/twitter-kit-android/wiki/Getting-Started
+https://github.com/twitter/twitter-kit-android/wiki/Log-In-with-Twitter
+
+
+Get SHA1 key fingerprint:
+keytool -exportcert -list -v -alias androidappskeystore -keystore %USERPROFILE%\AndroidStudioProjects\keystore.jks
+
+signingReport gradle task
+
+
+Firebase Realtime Database
+
+https://firebase.google.com/docs/database/android/start/
+https://github.com/firebase/quickstart-android/tree/master/database
--- app/src/main/java/com/sergiocruz/capstone/ui/PagerFragment.java
+++ app/src/main/java/com/sergiocruz/capstone/ui/PagerFragment.java
@@ -2,21 +2,45 @@ package com.sergiocruz.capstone.ui;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.sergiocruz.capstone.R;
 import com.sergiocruz.capstone.databinding.FragmentPagerBinding;
+
 
 
 /**
  * Main Fragment with App content, a ViewPager with bottom navigation in this case.
  */
 public class PagerFragment extends Fragment {
+
+    private FragmentPagerBinding binding;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    binding.message.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    binding.message.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    binding.message.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     public PagerFragment() {
         // Required empty public constructor
@@ -31,19 +55,22 @@ public class PagerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate view and obtain an instance of the binding class.
-        FragmentPagerBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pager, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pager, container, false);
 
         // Specify the current fragment as the lifecycle owner.
         binding.setLifecycleOwner(this);
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         binding.buttonLogout.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
+            LoginManager.getInstance().logOut();
+
             getActivity().getSupportFragmentManager()
                     .beginTransaction()
                     .add(R.id.main_fragment_container, new LoginFragment(), LoginFragment.class.getSimpleName())
                     .commit();
         });
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         return binding.getRoot();
     }

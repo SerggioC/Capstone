5758d084e95c97b7392c7dd70793364c96798835
SerggioC <sergiocrz@gmail.com>
2018-06-20 02:20:35 +0100

    tooling...

--- app/src/main/java/com/sergiocruz/capstone/ui/PagerFragment.java
+++ app/src/main/java/com/sergiocruz/capstone/ui/PagerFragment.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.sergiocruz.capstone.R;
 import com.sergiocruz.capstone.databinding.FragmentPagerBinding;
-
 
 
 /**
@@ -29,13 +35,13 @@ public class PagerFragment extends Fragment {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    binding.message.setText(R.string.title_home);
+                    binding.messageTextview.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    binding.message.setText(R.string.title_dashboard);
+                    binding.messageTextview.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    binding.message.setText(R.string.title_notifications);
+                    binding.messageTextview.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -72,7 +78,30 @@ public class PagerFragment extends Fragment {
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        // setup the menu and toolbar
+        setHasOptionsMenu(true);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(binding.toolbarLayout.toolbar);
+
+        Glide.with(this)
+                .load(R.mipmap.ic_launcher)
+                .apply(RequestOptions.circleCropTransform())
+                .into(binding.toolbarLayout.userIcon)
+                .onLoadFailed(ContextCompat.getDrawable(getContext(), R.drawable.ic_user_icon_48dp));
+
+        binding.toolbarLayout.userIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Should Open Account Drawer", Toast.LENGTH_LONG).show();
+            }
+        });
+
         return binding.getRoot();
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_options, menu);
     }
 
 
